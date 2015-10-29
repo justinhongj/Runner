b71dbb8782d600403545d755c8fc530b27d31bb5
Justin Hong <justinhong.j@gmail.com>
2015-10-28 17:30:36 -0700

    add player movement, penalty, sprite.kill

--- public/js/app.js
+++ public/js/app.js
@@ -14,13 +14,14 @@ function preload() {
 	game.load.spritesheet('scott1', 'images/scott1.png', 58, 76);
 	game.load.spritesheet('scott2', 'images/scott2.png', 58, 76);
 	game.load.spritesheet('floor', 'images/floor1.png', 250, 33);
-	game.load.image('line', 'images/blank_line.png');
+	game.load.image('line', 'images/purp_line.png');
 	game.load.image('spikeUp', 'images/spike_up.png');
 	game.load.image('spikeDown', 'images/spike_down.png');
 
 }
 
 function create() {
+
 	////////////////
 	// BACKGROUND
 	////////////////
@@ -32,21 +33,26 @@ function create() {
 	background.animations.add('streak', [0, 1, 2, 3, 2, 1], 10, true);
 	background.animations.play('streak');
 
+	// group for all spikes (used later for 'collision')
+	spikes = game.add.group();
+
 	////////////////
 	// TOP SCREEN
 	////////////////
 
 	floor = game.add.sprite(0, 140, 'floor');
-	floor.scale.y = game.rnd.realInRange(0.3, 0.3);
+	floor.scale.y = 0.3;
 	floor.width = game.width;
 
 	floor.animations.add('move', [0, 1, 2, 3, 4, 5], 10, true);
 	floor.animations.play('move');
 
+
 	line1 = game.add.sprite(0, 145, 'line');
 	line1.width = game.width;
 	game.physics.arcade.enable(line1);
 	line1.body.immovable = true;
+
 
 	scott1 = game.add.sprite(game.width/4, 90, 'scott1');
 	scott1.scale.x = 0.5;
@@ -59,21 +65,35 @@ function create() {
 	game.physics.arcade.enable(scott1);
 	scott1.body.gravity.y = 900;
 
+
+	spawnSpikeUp1();
+
+	(function loop() {
+		var rand = Math.round(Math.random() * 1000) + 600;
+		setTimeout(function() {
+			spawnSpikeUp1();
+			loop();
+		}, rand);
+	}());
+
+
 	////////////////
 	// BOTTOM SCREEN
 	////////////////
 
 	floor = game.add.sprite(0, 287, 'floor');
-	floor.scale.y = game.rnd.realInRange(0.3, 0.3);
+	floor.scale.y = 0.3;
 	floor.width = game.width;
 
 	floor.animations.add('move', [0, 1, 2, 3, 4, 5], 10, true);
 	floor.animations.play('move');
 
+
 	line2 = game.add.sprite(0, 292, 'line');
 	line2.width = game.width;
 	game.physics.arcade.enable(line2);
 	line2.body.immovable = true;
+ 
 
 	scott2 = game.add.sprite(game.width/4, 237, 'scott2');
 	scott2.scale.x = 0.5;
@@ -87,9 +107,51 @@ function create() {
 	scott2.body.gravity.y = 950;
 
 
+	spawnSpikeUp2();
+
+	(function loop() {
+		var rand = Math.round(Math.random() * 1000) + 600;
+		setTimeout(function() {
+			spawnSpikeUp2();
+			loop();
+		}, rand);
+	}());
+
+
+	line = game.add.sprite(0, 0, 'line');
+	line.scale.x = 0.01;
+	line.scale.y = game.height;
+	game.physics.arcade.enable(line);
+
 }
 
+
+function spawnSpikeUp1() {
+	spikeUp1 = game.add.sprite(game.width, 91, 'spikeUp');
+	spikeUp1.scale.y = 0.22;
+	spikeUp1.scale.x = 0.5;
+	game.physics.arcade.enable(spikeUp1);
+	spikeUp1.body.velocity.x = -600;
+	spikes.add(spikeUp1);
+}
+
+function spawnSpikeUp2() {
+	spikeUp2 = game.add.sprite(game.width, 238, 'spikeUp');
+	spikeUp2.scale.y = 0.22;
+	spikeUp2.scale.x = 0.5;
+	game.physics.arcade.enable(spikeUp2);
+	spikeUp2.body.velocity.x = -600;
+	spikes.add(spikeUp2);
+}
+
+function player1Win() {
+	spikes.kill();
+	text = game.add.text(20, 20, 'Player 1 Wins!!', {fill: '#ffffff'});
+}
+
+
 function update() {
+
 	////////////////
 	// COLLISION
 	////////////////
@@ -97,10 +159,12 @@ function update() {
 	game.physics.arcade.collide(scott1, line1);
 	game.physics.arcade.collide(scott2, line2);
 
+
 	////////////////
-	// JUMPING
+	// MOVEMENT
 	////////////////
 
+	// Player 1 Movement
 	if (game.input.keyboard.isDown(Phaser.Keyboard.Q)) {
 		if (scott1.body.velocity.y === 0) {
 			scott1.animations.play('jump');
@@ -112,14 +176,66 @@ function update() {
 		scott1.animations.play('run');
 	}
 
+	if (scott1.body.position.x < game.width/4) {
+		scott1.body.velocity.x = 20;
+	}
+
+	// Player 2 Movement
 	if (game.input.keyboard.isDown(Phaser.Keyboard.P)) {
 		if (scott2.body.velocity.y === 0) {
 			scott2.animations.play('jump');
 			scott2.body.velocity.y = -370;
 		}
 	}
-
 	if (scott2.body.velocity.y === 0) {
 		scott2.animations.play('run');
 	}
+
+	if (scott2.body.position.x < game.width/4) {
+		scott2.body.velocity.x = 20;
+	}
+
+
+	////////////////
+	// PENALTY
+	////////////////
+
+	// Player 1 Penalty
+	if (game.physics.arcade.overlap(scott1, spikes)) {
+		scott1.body.velocity.x = -300;
+		window.setTimeout(scott1Normalize, 200);
+	}
+	function scott1Normalize() {
+		scott1.body.velocity.x = 0;
+	}
+
+	if (game.physics.arcade.overlap(scott1, line)) {
+		scott1.kill();
+	}
+
+	// Player 2 Penalty
+	if (game.physics.arcade.overlap(scott2, spikes)) {
+		scott2.body.velocity.x = -300;
+		window.setTimeout(scott2Normalize, 200);
+	}
+	function scott2Normalize() {
+		scott2.body.velocity.x = 0;
+	}
+
+	if (game.physics.arcade.overlap(scott2, line)) {
+		scott2.kill();
+	}
+
+
+	////////////////
+	// GAMEOVER
+	////////////////
+
+	if (!(scott1.alive)) {
+		player2Win();
+	}
+	if (!(scott2.alive)) {
+		player1Win();
+	}
 }
+
--- public/js/preload.js
+++
@@ -1,8 +0,0 @@
-var Preload = {
-
-	preload: function() {
-		game.load.spritesheet('scottRunning', 'public/images/scott.png', 108, 240);
-		var loadingBar = game.add.sprite(160, 240, 'loadingBar');
-
-	}
-};
